93649aa98d00915c0648184f27da622bf1990b61
Alade Yessoufou <aladecharaf23@gmail.com>
2024-12-15 20:03:44 +0100

    move boot to install

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
-# Exit immediately if a command exits with a non-zero status
 set -e
 
+ascii_art='_________.__                                  
+ /   _____/|  |__ _____    ____    ____   ____  
+ \_____  \ |  |  \\__  \  /    \  / ___\ /  _ \ 
+ /        \|   Y  \/ __ \|   |  \/ /_/  >  <_> )
+/_______  /|___|  (____  /___|  /\___  / \____/ 
+        \/      \/     \/     \//_____/         '
+
+echo -e "$ascii_art"
+echo "=> Shango is Linux base tools!"
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
+
+echo "Cloning Shango..."
+rm -rf ~/.local/share/shango
+git clone https://github.com/devalade/shango.git ~/.local/share/shango >/dev/null
+if [[ $SHANGO_REF != "main" ]]; then
+  cd ~/.local/share/shango
+  git fetch origin "${SHANGO_REF:-stable}" && git checkout "${SHANGO_REF:-stable}"
+  cd -
+fi
+
+echo "Installation starting..."
+echo ""
 echo "Installing gum for better experience."
 source ~/.local/share/shango/install/terminal/required/app-gum.sh >/dev/null
